9604deebe94fc85386f044a2237d1db95a5dc057
lilin <565387073@qq.com>
2025-08-26 13:46:42 +0800

    修改测试

---
+++ test.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+
+client = OpenAI(
+    base_url='https://api-inference.modelscope.cn/v1/',
+    api_key='a8d687df-', # ModelScope Token
+)
+
+response = client.chat.completions.create(
+    model='deepseek-ai/DeepSeek-R1-0528', # ModelScope Model-Id
+    messages=[
+        {
+            'role': 'user',
+            'content': '你好'
+        }
+    ],
+    stream=True
+)
+done_reasoning = False
+for chunk in response:
+    reasoning_chunk = chunk.choices[0].delta.reasoning_content
+    answer_chunk = chunk.choices[0].delta.content
+    if reasoning_chunk != '':
+        print(reasoning_chunk, end='',flush=True)
+    elif answer_chunk != '':
+        if not done_reasoning:
+            print('\n\n === Final Answer ===\n')
+            done_reasoning = True
+        print(answer_chunk, end='',flush=True)
